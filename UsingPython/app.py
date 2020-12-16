2decd13778a11126c435135e2cb6e272f6910d89
Didi Yang <bingdyang@uiowa.edu>
2020-12-16 12:43:44 -0600

    add book_id

--- UsingPython/app.py
+++ UsingPython/app.py
@@ -22,7 +22,7 @@ def search():
     records = []
     resultOne = books.find_one({"$text": {"$search" : '\"query\"'}},{'original_title':1,'book_id':1, 'authors':1,'original_publication_year':1,
                             'small_image_url':1, 'average_rating':1,'isbn':1, 'ratings_count':1, '_id':0})
-    for result in books.find({"$text": {"$search" : query}},{'original_title':1,'authors':1,'original_publication_year':1,
+    for result in books.find({"$text": {"$search" : query}},{'original_title':1,'book_id':1,'authors':1,'original_publication_year':1,
                             'small_image_url':1, 'average_rating':1,'isbn':1, 'ratings_count':1, '_id':0}):
         records.append(result)
 
